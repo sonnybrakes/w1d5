5e2486cae3677fad2c2f840d3695a434fbfa63e7
Michel Frechette <mifrec@hotmail.com>
2015-11-23 11:03:42 -0500

    Create a class

---
+++ contact.rb
@@ -0,0 +1,13 @@
+class Contact
+  attr_accessor :first_name, :last_name, :email, :notes
+
+  # attr_accessor creates these methods
+
+  # def first_name
+  #   @first_name
+  # end
+
+  # def first_name=(new_first_name)
+  #   @first_name = new_first_name
+  # end
+end
