7e8e514fad564d479a727490ad5ef6ec8d3e9ec7
Michel Frechette <mifrec@hotmail.com>
2015-11-23 12:22:51 -0500

    Options w/braces

--- contact.rb
+++ contact.rb
@@ -1,7 +1,7 @@
 class Contact
   attr_accessor :first_name, :last_name, :email, :notes
 
-  def initialize(options)
+  def initialize(options = {})
     @first_name = options[:first_name]
     @last_name = options[:last_name]
     @email = options[:email]
