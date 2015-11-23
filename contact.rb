89ab96dda2ec5d5acbd234aa255b08a44477b6f7
Michel Frechette <mifrec@hotmail.com>
2015-11-23 13:15:54 -0500

    Add @@class level variable

--- contact.rb
+++ contact.rb
@@ -2,6 +2,8 @@ class Contact
   attr_reader :id
   attr_accessor :first_name, :last_name, :email, :notes
 
+  @@contacts = []
+
   def initialize(first_name, last_name, options = {})
     @first_name = first_name
     @last_name = last_name
