719df72de761e1f83c07c8413bac4c14df58185f
Michel Frechette <mifrec@hotmail.com>
2015-11-23 11:40:20 -0500

    Initialize

--- contact.rb
+++ contact.rb
@@ -1,6 +1,16 @@
 class Contact
   attr_accessor :first_name, :last_name, :email, :notes
 
+  def initialize(first_name, last_name, email, notes = nil)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @notes = notes
+  # why initialize? when does initialize get run?
+  # initialize gets run on dot new
+  # eg.> michel = Contact.new
+  end
+
   # attr_accessor creates these methods
 
   # def first_name
