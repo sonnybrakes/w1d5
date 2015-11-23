8c4a2320e22c2acb6ba84db5b0014e392ad8b45e
Michel Frechette <mifrec@hotmail.com>
2015-11-23 14:38:03 -0500

    fix broken code

--- contact.rb
+++ contact.rb
@@ -6,16 +6,17 @@ class Contact
   @@id = 1
 
   def initialize(first_name, last_name, options = {})
+    @id = @@id
     @first_name = first_name
     @last_name = last_name
     @email = options[:email]
     @notes = options[:notes]
+
+    @@id += 1
   end
 
   def self.create(first_name, last_name, options = {})
     new_contact = Contact.new(first_name, last_name, options)
-    new_contact.id = @@id
-    @@id += 1
     @@contacts << new_contact
   end
   # attr_accessor creates these methods
