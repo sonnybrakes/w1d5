c33854a25d730047ea1186a125e071de01c843c1
Michel Frechette <mifrec@hotmail.com>
2016-01-18 02:54:04 -0500

    use a hash to pass optional values

--- contact.rb
+++ contact.rb
@@ -1,13 +1,13 @@
 # so what we're going to do is, we want to approach this in an object oriented way, so first we'll make a file and call it contact.rb. so the first step, we're going to make a contact class because what we want to do is we want to represent the idea of a contact in our application. so that's the first thing that we need to kind of wrap our minds around of. every application we're going to build from this point forward is going to be object oriented so we need to think in terms of objects, rather than functions or methods or anything like that. we need to build out objects that represent what it is that we want them to do.
 # the next thing that we need to do and it's something that i'll repeat, is that what your goal is   when your building out a class or an object is you need to keep that object like you have to have a crystaline focus on what the responsibilities of each object in the system are and you don't want things to cross over you don't want responsibilities to leak into a class where it's doing something that it wasn't intended to do. so if i make a contact class, let's call it Contact
 class Contact
-  attr_accessor :first_name :last_name :email :note
+  attr_accessor :first_name, :last_name, :email, :note
 
-  def initialize(first_name, last_name, email, note)
+  def initialize(first_name, last_name, option = {})
     @first_name = first_name
     @last_name = last_name
-    @email = email
-    @note = note
+    @email = options[:email]
+    @note = options[:note]
   end
 
   # getter
@@ -21,7 +21,7 @@ class Contact
 
 end
 
-new_contact = Contact.new("Michel", "Frechette", "mifrec@hotmail.com", "Swell fella")
+# new_contact = Contact.new("Michel", "Frechette", "mifrec@hotmail.com", "Swell fella")
 # then before we write any of the code, what are going to be the responsibilities of the Contact class? store the attributes of a Contact, definitely, that's the main responsibility of this Contact class. so let's set that up.
 # let's say we have five attributes: id, first name, last name, email, and a note about each user. so how are we going to set up those attributes for this thing? i heard a couple of people say initialize. we can do initialize, sure, right now if we do new_contact = Contact.new that'll work, right? we have a class, that's great.
 # we want to be able to store those five attributes in this object. so how are we going to do that? so we're going to make an initialize method is what we're saying.
@@ -34,3 +34,13 @@ new_contact = Contact.new("Michel", "Frechette", "mifrec@hotmail.com", "Swell fe
 # attribute accessor, attr_accessor, and what does it do? attr_accessor creates the getter and setter methods.
 # as a review let's type out one of these getters and setters methods for the first name. so what is that going to look like? define first_name to define the getter to return an instance variable. all we want to do is return the value stored in the @first_name instance variable. that's what we're doing, we're reading from that variable. the last line of execution is of our method is the return value. so what about the the setter? what does that look like?
 # the getter and setter methods are replaced using the attr_accessor method. when we use attr_accessor the getter and setter are written out for us inside the method, this saves us a lot of time.
+# okay now let's talk about a circumstance where we don't know a person's email or note details yet. so we want to add a Contact to our Contact list. how do we make it so that we don't have to provide the email and the note? or just the note or maybe all of it's optional. what do we want to do here?
+# we could take out the initialize method that way we don't need to pass any value and we can set the values later on. so what happens if we do that? let's try it out.
+# an empty Contact is never useful in this application, is it? i don't know, is it? the reason we would use initialize is to say that if we don't pass in information to this object, the object is useless. the object cannot perform its function. in this case, having an empty contact, is that a valuable thing that we're able to do? maybe, maybe not.
+# we want to be able to allow the user to create a new Contact and pass in all the information when we first create a Contact, but we don't want it to be restricted to that. maybe there are use cases where we want to create an empty contact and then populating it after the fact is valuable. we can actually find a happy medium here.
+# a happy medium. instead of using four named parameters here, what we're going to do is, we're going to use one parameter and then assume that that one parameter is a hash of key value pairs. first_name is this, last_name is this, email is this, note is this. notice that when we switch the attribute to a hash we don't have to all of the keys, right? we can have some of them and not others, right? if we look up a key in a hash and it doesn't have that key, what is the return value? nil, right? it doesn't exist. it doesn't crash the program, it just sets that value to nil, which it would be, if you never set it aways, right?
+# so what we can do here is instead of using: first_name, last_name, email, and note. let's say we want to require: first_name and last_name, but the other two are optional. what we'll do is say options equals an empty hash and then for the email and note we'll say options square brackets :email and options square brackets :note. so if those things are in the options hash the value will be set. if those things are not in the options hash the value will be set to nil, which is the default value for those variables anyways.
+# so you pass the key in between the square brackets and it returns the value for that key. that's how you dereference a key in a hash. it's exactly the same syntax as an array except that you pass in the key, rather than an index integer.
+# so we could have replaced all four, but now we actually now have it so that we must pass in the  first_name and last_name and optionally we can pass in the email and note.
+# we'll see this pattern very frequently. this pattern comes up quite a lot in ruby in places where it doesn't look right. if we have a method that takes more than three arguments usually that's something weird going on, right? and usually there's a way we can transform it into something that takes one argument that's a hash of key value pairs.
+# we don't always want to replace our arguments with hashes, sometimes there are required arguments where an object really can't do its job unless we pass the information and we want those to be manditory. we need to enforce that information to be passed through.
